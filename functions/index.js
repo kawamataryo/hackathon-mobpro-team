50684ebc67336e4819028d334a557540852dbf41
Satoshi Ebisawa <e.satoshi@gmail.com>
2019-09-14 14:09:04 +0900

    Add submitAnswer.js (scaffold)

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.submitAnswer = functions.https.onRequest((request, response) => {
+  if (request.method !== "POST") {
+    return response.status(405).end()
+  }
+
+
+  const result = {
+    result: 'ok'
+  }
+  return response.status(201).json(result)
+})
